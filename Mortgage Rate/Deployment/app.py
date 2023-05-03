782917ff864c3e6278fa21ffc9ea49b2e7b45a6d
Hiren Patel <78246319+hiruop@users.noreply.github.com>
2023-05-03 13:23:59 -0400

    Add files via upload

---
+++ Mortgage Rate/Deployment/app.py
@@ -0,0 +1,22 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("rf_model.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods=["POST"])
+def predict():
+    interest_rate = float(request.form["interest_rate"])
+    hpi_change = float(request.form["hpi_change"])
+    X = np.array([[interest_rate, hpi_change]])
+    prediction = model.predict(X)[0]
+    return render_template("index.html", prediction_text="Your mortgage rate will be {:.2f}%".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
